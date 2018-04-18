827d77854924a52aec3e07595c8baba336a8a8b5
J. Lewis <inquiries.lewis+dev@gmail.com>
2018-04-18 18:00:39 -0400

    Cut back on unnecessary code, added methods

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let game = {
+    win: ['0000', '1111'],
     board: {
         dimensions: {
             width: 8, // Max: 10
@@ -11,63 +12,53 @@ let game = {
     turnCount: 0,
     turnChange: () => game.turn = game.turn === 0 ? 1 : 0,
     evaluate: (x, y) => {
-        function horz(x, y) {
-            let seq = [];
-            seq.push(board[x][y]);
-            for (let x2 = x + 1; x2 < board.length; x2++) seq.push(board[x2][y]);
-            for (let x2 = x - 1; x2 >= 0; x2--) seq.unshift(board[x2][y]);
-            return seq;
-        }
-
-        function vert(x) {
-            return board[x]
-        };
-
-        function pos(x, y) {
-            let seq = [];
-            seq.push(board[x][y]);
-            for (let x2 = x + 1, y2 = y + 1; x2 < board.length && y2 < board[x2].length; x2++, y2++) seq.push(board[x2][y2]);
-            for (let x2 = x - 1, y2 = y - 1; x2 >= 0 && y2 >= 0; x2--, y2--) seq.unshift(board[x2][y2]);
-            return seq;
-        }
-
-        function neg(x, y) {
-            let seq = [];
-            seq.push(board[x][y]);
-            for (let x2 = x - 1, y2 = y + 1; x2 >= 0 && y2 < board[x2].length; x2--, y2++) seq.push(board[x2][y2]);
-            for (let x2 = x + 1, y2 = y - 1; x2 < board.length && y2 >= 0; x2++, y2--) seq.unshift(board[x2][y2]);
-            return seq;
-        }
         let execute = [
-            horz(x, y), vert(x), pos(x, y), neg(x, y)
+            (x, y) => game.board.literal[x],
+            (x, y) => {
+                let seq = [];
+                seq.push(game.board.literal[x][y]);
+                for (let x2 = x + 1; x2 < game.board.literal.length; x2++) seq.push(game.board.literal[x2][y]);
+                for (let x2 = x - 1; x2 >= 0; x2--) seq.unshift(game.board.literal[x2][y]);
+                return seq;
+            },
+            (x, y) => {
+                let seq = [];
+                seq.push(game.board.literal[x][y]);
+                for (let x2 = x + 1, y2 = y + 1; x2 < game.board.literal.length && y2 < game.board.literal[x2].length; x2++, y2++) seq.push(game.board.literal[x2][y2]);
+                for (let x2 = x - 1, y2 = y - 1; x2 >= 0 && y2 >= 0; x2--, y2--) seq.unshift(game.board.literal[x2][y2]);
+                return seq;
+            },
+            (x, y) => {
+                let seq = [];
+                seq.push(game.board.literal[x][y]);
+                for (let x2 = x - 1, y2 = y + 1; x2 >= 0 && y2 < game.board.literal[x2].length; x2--, y2++) seq.push(game.board.literal[x2][y2]);
+                for (let x2 = x + 1, y2 = y - 1; x2 < game.board.literal.length && y2 >= 0; x2++, y2--) seq.unshift(game.board.literal[x2][y2]);
+                return seq;
+            }
         ];
         let str = '';
         for (let i = 0; i < execute.length; i++) {
-            str += execute[i].join('');
+            str += execute[i](x, y).join('');
             if (i !== execute.length - 1) str += '/';
         }
         return str;
     }
 };
-const board = game.board.literal,
-    boardHeight = game.board.dimensions.height,
-    boardWidth = game.board.dimensions.width;
-for (let i = 0; i < boardWidth; i++) board.push([]);
+for (let i = 0; i < game.board.dimensions.width; i++) game.board.literal.push([]);
 
 document.body.addEventListener('keyup', (e) => {
     if (!game.end) {
-        let key,
-            comp = ['0000', '1111'];
+        let key;
         if (e.keyCode >= 48 && e.keyCode <= 57) {
             for (let i = 0; i < 10; i++) {
                 key = e.keyCode === i + 49 ? i : undefined;
                 if (typeof key === 'undefined') key = 9;
                 else break;
             }
-            if (key < boardWidth && board[key].length < boardHeight) {
-                board[key].push(game.turn), game.turnCount++;
+            if (key < game.board.dimensions.width && game.board.literal[key].length < game.board.dimensions.height) {
+                game.board.literal[key].push(game.turn), game.turnCount++;
                 if (game.turnCount >= 4)
-                    if (game.evaluate(key, board[key][board[key].length - 1]).includes(comp[game.turn])) {
+                    if (game.evaluate(key, game.board.literal[key][game.board.literal[key].length - 1]).includes(game.win[game.turn])) {
                         console.log('Player ' + (game.turn + 1) + ' Won.'), game.end = 1;
                     } else game.turnChange();
                 else game.turnChange();
